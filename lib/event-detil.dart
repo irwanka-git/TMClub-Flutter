4f8061610a5d74c838836c216761b036573b97a2
Irwan Kurniawan <drive.irwanka@gmail.com>
2022-12-23 14:02:42 +0700

    Update event-detil.dart

--- lib/event-detil.dart
+++ lib/event-detil.dart
@@ -217,7 +217,7 @@ class _EventDetilScreenState extends State<EventDetilScreen> {
                       ? GFCard(
                           margin: EdgeInsets.all(0),
                           padding: EdgeInsets.all(0),
-                          content: isMyEvent.value ==true ? buildActionAdmin() :buildLoadingPanelCard(),
+                          content: isMyEvent.value ==true ? buildActionAdmin() :Container(),
                         )
                       : onCheckMyEvent.value == true &&
                               authController.user.value.role == "admin"
